70b60beddf089727016316dd744fd0f80f7e1b99
OdongAlican <sandieo.2020@gmail.com>
2020-05-10 19:07:15 +0300

    adds gem files and creates the url method

---
+++ Scrapper.rb
@@ -0,0 +1,21 @@
+require 'httparty'
+require 'nokogiri'
+require 'byebug'
+
+def scraper
+    url = "https://sfbay.craigslist.org/search/hhh?s=0"
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
+    housing_list = parsed_page.css('li.result-row')
+    housing_list.each do |housing|
+         house = {
+             title: housing.css('a.result-title').text,
+             price: housing.css('span.result-price')[0].text,
+             url: housing.css('a')[0].attributes["href"].value,
+             date: housing.css('time.result-date').text
+         }
+         byebug
+    end
+end
+
+scraper

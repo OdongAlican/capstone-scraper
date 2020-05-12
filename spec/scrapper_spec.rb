63b14fc7545543cc28f53bbedbda82dd45a1b6e6
OdongAlican <sandieo.2020@gmail.com>
2020-05-12 12:12:50 +0300

    adds test for the private method page_number

--- spec/scrapper_spec.rb
+++ spec/scrapper_spec.rb
@@ -5,7 +5,7 @@ require_relative '../lib/scrapper.rb'
 describe 'Scraper' do
   let(:scraper) { Scraper.new('https://example.com') }
   let(:loop) { Loop.new(200, 1) }
-  
+
   describe '#run' do
     it 'calls the run method with a true value' do
       allow(scraper).to receive(:run).and_return(true)
@@ -17,5 +17,15 @@ describe 'Scraper' do
       expect(scraper.run).not_to eql(false)
     end
   end
-end
 
+  describe '#page_number' do
+    it 'allows private method page_number to return true when it recieves a true value' do
+      allow_any_instance_of(Scraper).to receive(:page_number).and_return(true)
+      expect(scraper.page_number).to eql(true)
+    end
+    it 'allows private method page_number not to return true when it recieves a true value' do
+      allow_any_instance_of(Scraper).to receive(:page_number).and_return(true)
+      expect(scraper.page_number).not_to eql(false)
+    end
+  end
+end
